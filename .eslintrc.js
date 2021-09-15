dc18e97b9aa7e46684ff7b947d2d97231d286381
Lucas Tagliani Aguiar <lucas.tagliani@outlook.com>
2021-09-15 02:29:27 -0300

    chore: add commitlint + conventional commits on commit-msg hook (#6)

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'react-app',
+    'react-app/jest',
+    'airbnb',
+    'airbnb-typescript',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    semi: ['error', 'never'],
+    '@typescript-eslint/semi': ['error', 'never'],
+    'unused-imports': 0,
+    'no-unused-vars': 0,
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      parserOptions: {
+        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['unused-imports'],
+}
