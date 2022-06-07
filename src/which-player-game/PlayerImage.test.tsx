0ad8c944623436bb66a3d085cfd027a96a859946
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-06-07 18:15:29 -0300

    feat(which-player-game): creates PlayerImage component

---
+++ src/which-player-game/PlayerImage.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { ByRoleOptions, render, screen } from '@testing-library/react'
+import PlayerImage from './PlayerImage'
+
+const getPlayerImage = (options?: ByRoleOptions): HTMLElement => screen.getByRole('img', options)
+
+const defaultProps = {
+  altText: 'player image',
+  playerId: '123',
+
+}
+
+describe('<PlayerImage />', () => {
+  it('should render player image with passed altText', () => {
+    render(<PlayerImage {...defaultProps} />)
+    expect(getPlayerImage({ name: 'player image' })).toBeInTheDocument()
+  })
+
+  it('should render player image with built src', () => {
+    render(<PlayerImage {...defaultProps} />)
+    const expectedSrc = 'https://cdn.nba.com/headshots/nba/latest/1040x760/123.png'
+    expect(getPlayerImage().getAttribute('src')).toBe(expectedSrc)
+  })
+
+  it('should render player image with passed className', () => {
+    render(<PlayerImage {...defaultProps} className="my-custom-class-name" />)
+    expect(getPlayerImage().getAttribute('class')).toBe('my-custom-class-name')
+  })
+
+  it('should render player image with passed styles', () => {
+    render(<PlayerImage {...defaultProps} overrideStyles={{ border: '1px solid red' }} />)
+    expect(getPlayerImage()).toHaveStyle({ border: '1px solid red' })
+  })
+})
---
+++ src/which-player-game/PlayerImage.tsx
@@ -0,0 +1,29 @@
+import React, { CSSProperties } from 'react'
+
+type PlayerImageProps = {
+  altText: string
+  playerId: string,
+  className?: string
+  overrideStyles?: CSSProperties
+}
+
+const defaultStyles = {
+  height: '400px',
+}
+
+const BASE_URL_PLAYER_IMAGE = 'https://cdn.nba.com/headshots/nba/latest/1040x760/'
+const FILE_EXTENSION_PLAYER_IMAGE = 'png'
+
+const PlayerImage = ({
+  playerId, altText, className, overrideStyles,
+}: PlayerImageProps) => {
+  const style = {
+    ...defaultStyles,
+    ...overrideStyles,
+  }
+
+  return (
+    <img alt={altText} src={`${BASE_URL_PLAYER_IMAGE}${playerId}.${FILE_EXTENSION_PLAYER_IMAGE}`} className={className} style={style} />
+  )
+}
+export default PlayerImage
