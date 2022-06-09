301d1aad060e6dafdc5980aa6fd87c928c088b59
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-06-09 04:54:22 -0300

    refactor: rename folder to core-components

--- src/which-player-game/WhichPlayerGame.tsx
+++ src/which-player-game/WhichPlayerGame.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Button from '../components/button/Button'
+import Button from '../core-components/button/Button'
 import PlayerImage from './PlayerImage'
 
 const WhichPlayerGame = () => {
