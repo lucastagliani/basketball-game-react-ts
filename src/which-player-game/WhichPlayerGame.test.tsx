2055386fedcbc03e33f05443e5c42d2908258cbd
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-06-10 15:46:00 -0300

    feat(which-player-game): click answer behavior

--- src/which-player-game/WhichPlayerGame.test.tsx
+++ src/which-player-game/WhichPlayerGame.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import WhichPlayerGame from './WhichPlayerGame'
+import userEvent from '@testing-library/user-event'
 
 describe('<WhichPlayerGame />', () => {
   it('should render a game title', () => {
@@ -16,5 +17,27 @@ describe('<WhichPlayerGame />', () => {
   it('should render 4 possible answers', () => {
     render(<WhichPlayerGame />)
     expect(screen.getAllByRole('button').length).toBe(4)
+  })
+
+  it('should give feedback to user when answer is right', () => {
+    // TODO: mock data from API
+    render(<WhichPlayerGame />)
+    const rightAnswer = screen.getByText('James Harden')
+    userEvent.click(rightAnswer)
+    expect(rightAnswer).toHaveStyle({
+      'background-color': 'green',
+      color: 'white'
+    })
+  })
+
+  it('should give feedback to user when answer is incorrect', () => {
+    // TODO: mock data from API
+    render(<WhichPlayerGame />)
+    const rightAnswer = screen.getByText('James Harden')
+    userEvent.click(rightAnswer)
+    expect(rightAnswer).toHaveStyle({
+      'background-color': 'red',
+      color: 'white'
+    })
   })
 })
