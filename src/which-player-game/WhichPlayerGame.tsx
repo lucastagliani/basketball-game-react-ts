eadfd612dbc2abdf125270a63932fb9c7d275d76
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-07-08 22:50:42 -0300

    refactor(which-player-game): update buttons border rule

--- src/which-player-game/WhichPlayerGame.tsx
+++ src/which-player-game/WhichPlayerGame.tsx
@@ -2,14 +2,23 @@ import React, { useState } from 'react'
 import Button from '../core-components/button/Button'
 import PlayerImage from './PlayerImage'
 
+const getNewQuestion = () => ({
+  correctAnswerKey: 1628391,
+  image: 'https://cdn.nba.com/headshots/nba/latest/1040x760/1628391.png',
+  alternativeOptions: [],
+})
+
 const WhichPlayerGame = () => {
-  const [alternativies, setAlternativies] = useState([{ key: '201935', value: 'James Harden' }, { key: '1626164', value: 'Devin Booker' }, { key: '202710', value: 'Jimmy Butler' }, { key: '203076', value: 'Antonhy Davis' }])
-  const [rightAnswer, setRightAnswer] = useState("201935")
+  const [alternativies, setAlternativies] = useState([{ key: 201935, value: 'James Harden' }, { key: 1626164, value: 'Devin Booker' }, { key: 202710, value: 'Jimmy Butler' }, { key: 203076, value: 'Antonhy Davis' }])
+  const [rightAnswer, setRightAnswer] = useState(201935)
   const [userAnswer, setUserAnswer] = useState()
 
   const handleOnClick = (event: any) => {
     setUserAnswer(event.target.value)
 
+    const newQuestion = getNewQuestion()
+    setRightAnswer(newQuestion.correctAnswerKey)
+    setAlternativies(newQuestion.alternativeOptions)
     // get new player, setAlternativies, setRightAnswer
   }
 
@@ -22,24 +31,18 @@ const WhichPlayerGame = () => {
           let stylesToOverride
           if (!!userAnswer && a.key === userAnswer) {
             stylesToOverride = {
-              backgroundColor: rightAnswer === userAnswer ? 'green' : 'red',
-              color: 'white'
+              border: `4px solid ${rightAnswer === userAnswer ? 'green' : 'red'}`,
             }
-            // if (rightAnswer === userAnswer ) {
-            //   stylesToOverride = {
-            //     backgroundColor: 'green',
-            //     color: 'white'
-            //   }
-            // } else {
-            //   stylesToOverride = {
-            //     backgroundColor: 'red',
-            //     color: 'white'
-            //   }
-            // }
           }
 
           return (
-            <Button key={a.key} value={a.key} text={a.value} onButtonClick={handleOnClick} overrideStyles={stylesToOverride}/>
+            <Button
+              key={a.key}
+              value={a.key.toString()}
+              text={a.value.toString()}
+              onButtonClick={handleOnClick}
+              overrideStyles={stylesToOverride}
+            />
           )
         })}
       </div>
