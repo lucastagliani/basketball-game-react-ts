7fd8b817e3349590d222aa5fdbe6470a10c75203
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-07-22 23:49:47 -0300

    feat(core-components): creates Score component

---
+++ src/core-components/score/Score.test.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Score from './Score'
+
+
+describe('<Score />', () => {  
+  it('should render 2 / 3 when 3 attempts and 2 correct', () => {
+    render(<Score attempts={3} correct={2} />)
+    expect(screen.getByText('2 / 3')).toBeInTheDocument()
+  })
+
+  it('should render with default styles', () => {
+    render(<Score attempts={3} correct={2} />)
+
+    const scoreElement = screen.getByText('2 / 3')
+    expect(scoreElement.parentElement).toHaveStyle({
+      padding: '1rem',
+      fontSize: '2rem',
+      color: '#333'
+    })
+  })
+
+  it('should render text before score when provided', () => {
+    render(<Score attempts={3} correct={2} textBeforeScore='Score: '/>)
+    expect(screen.getByText('Score:')).toBeInTheDocument()
+  })
+
+  it('should render with custom styles', () => {
+    const props = {
+      attempts: 1,
+      correct: 0,
+      textBeforeScore: 'Your score is: ',
+      containerStyles: {
+        border: '1px solid gold'
+      },
+      textStyles: {
+        color: '#000'
+      },
+      scoreStyles: {
+        fontSize: '16px',
+      },
+    }
+
+    render(<Score {...props} />)
+
+    const textElement = screen.getByText('Your score is:')
+    const scoreElement = screen.getByText('0 / 1')
+    
+    expect(textElement).toHaveStyle({color: '#000'})
+    expect(scoreElement).toHaveStyle({'font-size': '16px'})
+    expect(scoreElement.parentElement).toHaveStyle({'border':'1px solid gold'})
+  })
+})
---
+++ src/core-components/score/Score.tsx
@@ -0,0 +1,34 @@
+import React, { CSSProperties } from 'react'
+
+type ScoreProps = {
+  attempts: number
+  correct: number
+  textBeforeScore?: string
+  textStyles?: CSSProperties
+  scoreStyles?: CSSProperties
+  containerStyles?: CSSProperties
+}
+
+const defaultContainerStyles: CSSProperties = {
+  padding: '1rem',
+  fontSize: '2rem',
+  color: '#333'
+}
+
+const Score = ({
+  attempts, 
+  correct, 
+  textBeforeScore = '', 
+  scoreStyles, 
+  textStyles, 
+  containerStyles
+}: ScoreProps): JSX.Element => {
+  return (
+    <div style={{...defaultContainerStyles, ...containerStyles}}>
+      {textBeforeScore && <span style={textStyles}>{textBeforeScore}</span>}
+      <span style={scoreStyles}>{`${correct} / ${attempts}`}</span>
+    </div>
+  )
+}
+
+export default Score
