38691e21f35af2f9ea6131f1919409032efa24cd
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-07-29 02:53:17 -0300

    test(which-player-game): adds tests and mock axios

---
+++ src/which-player-game/questionsApiMockedData.ts
@@ -0,0 +1,23 @@
+export const questionsApiMockedData = {
+  sentence: 'Which basketball player is this?',
+  correctAnswerKey: 203999,
+  image: 'https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png',
+  alternativeOptions: [
+    {
+      key: 203999,
+      value: 'Nikola Jokic',
+    },
+    {
+      key: 1628381,
+      value: 'John Collins',
+    },
+    {
+      key: 1630532,
+      value: 'Franz Wagner',
+    },
+    {
+      key: 202695,
+      value: 'Kawhi Leonard',
+    },
+  ],
+}
