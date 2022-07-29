a159c51773abb330d87de4a295a3cdd2b6d03580
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-07-29 02:53:17 -0300

    test(which-player-game): adds tests and mock axios

--- src/which-player-game/WhichPlayerGame.test.tsx
+++ src/which-player-game/WhichPlayerGame.test.tsx
@@ -2,6 +2,19 @@ import React from 'react'
 import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import WhichPlayerGame from './WhichPlayerGame'
+import { questionsApiMockedData } from './questionsApiMockedData'
+
+jest.mock('axios', () => ({
+  get: () => {
+    return {
+      data: questionsApiMockedData,
+      status: 200,
+      statusText: 'Ok',
+      headers: {},
+      config: {},
+    }
+  },
+}))
 
 describe('<WhichPlayerGame />', () => {
   it('should render a game title', async () => {
@@ -13,38 +26,33 @@ describe('<WhichPlayerGame />', () => {
       expect(gameTitle).toBeInTheDocument()
     })
   })
-
   it('should render a player picture', async () => {
     render(<WhichPlayerGame />)
     const playerImage = screen.getByRole('img', { name: 'player' })
     await waitFor(() => {
       expect(playerImage).toBeInTheDocument()
-      // expect(screen.getByRole('button')).toBeInTheDocument()
     })
   })
-
-  xit('should render 4 possible answers', () => {
+  it('should render 4 possible answers', async () => {
     render(<WhichPlayerGame />)
-    expect(screen.getAllByRole('button').length).toBe(4)
-  })
-
-  xit('should give feedback to user when answer is right', () => {
-    // TODO: mock data from API
-    render(<WhichPlayerGame />)
-    const rightAnswer = screen.getByText('James Harden')
-    userEvent.click(rightAnswer)
-    expect(rightAnswer).toHaveStyle({
-      border: '4px solid green',
+    await waitFor(() => {
+      expect(screen.getAllByRole('button').length).toBe(4)
     })
   })
-
-  xit('should give feedback to user when answer is incorrect', () => {
-    // TODO: mock data from API
+  it('should give feedback to user when answer is right', async () => {
     render(<WhichPlayerGame />)
-    const rightAnswer = screen.getByText('Devin Booker')
-    userEvent.click(rightAnswer)
-    expect(rightAnswer).toHaveStyle({
-      border: '4px solid red',
+    await waitFor(() => {
+      const rightAnswer = screen.getByText('Nikola Jokic')
+      userEvent.click(rightAnswer)
+      expect(screen.getByText('1 / 1')).toBeInTheDocument()
+    })
+  })
+  it('should give feedback to user when answer is incorrect', async () => {
+    render(<WhichPlayerGame />)
+    await waitFor(() => {
+      const rightAnswer = screen.getByText('Kawhi Leonard')
+      userEvent.click(rightAnswer)
+      expect(screen.getByText('0 / 1')).toBeInTheDocument()
     })
   })
 })
