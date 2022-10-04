537ca8298f5b8a398b118620372dcbf0e5bf87be
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-10-04 18:12:13 -0300

    refactor(which-player-game): creates AnswerOptions component

---
+++ src/which-player-game/AnswerOptions/AnswerOptions.test.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import AnswerOptions from './AnswerOptions'
+
+describe('<AnswerOptions />', () => {
+  const onAnswerClickSpy = jest.fn()
+  const props = {
+    alternativies: [
+      {
+        key: 203999,
+        value: 'Nikola Jokic',
+      },
+      {
+        key: 1628381,
+        value: 'John Collins',
+      },
+      {
+        key: 1630532,
+        value: 'Franz Wagner',
+      },
+      {
+        key: 202695,
+        value: 'Kawhi Leonard',
+      }
+    ],
+    onAnswerClick: onAnswerClickSpy
+  }
+
+  beforeEach(() => {
+    render(<AnswerOptions {...props}/>)
+  })
+
+  it('should render all answer options provided', () => {
+    expect(screen.getAllByRole('button').length).toBe(4)
+  })
+
+  it('should call onAnswerClick when an answer option is clicked', () => {
+    const rightAnswer = screen.getByText('Kawhi Leonard')
+    userEvent.click(rightAnswer)
+    expect(onAnswerClickSpy).toBeCalledTimes(1)
+  })
+})
