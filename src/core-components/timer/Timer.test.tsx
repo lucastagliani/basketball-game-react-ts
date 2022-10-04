dd7b7ed4ae7a0637b087200348a80e292520777c
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-10-04 16:53:39 -0300

    refactor(timer): improve code readibility

--- src/core-components/timer/Timer.test.tsx
+++ src/core-components/timer/Timer.test.tsx
@@ -5,12 +5,12 @@ import Timer from './Timer'
 describe('<Timer>', () => {
   it('should start the initial clock as 00:00.000', () => {
     render(<Timer />)
-    expect(screen.getByText('00:00.0')).toBeInTheDocument()
+    expect(screen.getByText('00:00.00')).toBeInTheDocument()
   })
 
   it('should NOT have the initial state after started', async () => {
     render(<Timer isRunning={true} />)
-    await waitForElementToBeRemoved(() => screen.queryByText('00:00.0'))
-    expect(screen.queryByText('00:00.0')).not.toBeInTheDocument()
+    await waitForElementToBeRemoved(() => screen.queryByText('00:00.00'))
+    expect(screen.queryByText('00:00.00')).not.toBeInTheDocument()
   })
 })
