e75403d6391e179c5dfbad1453bf608e1c33b005
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-10-04 02:42:36 -0300

    fix: improve mobile experience

--- src/which-player-game/PlayerImage.tsx
+++ src/which-player-game/PlayerImage.tsx
@@ -1,4 +1,5 @@
 import React, { CSSProperties } from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 type PlayerImageProps = {
   altText: string
@@ -7,16 +8,22 @@ type PlayerImageProps = {
   overrideStyles?: CSSProperties
 }
 
-const defaultStyles = {
-  height: '400px',
+const getDefaultStyles = (isMobile: boolean): CSSProperties => {
+  return isMobile ? {
+    maxWidth: '100%',
+  } : {
+    height: '400px',
+  }
 }
 
 const BASE_URL_PLAYER_IMAGE = 'https://cdn.nba.com/headshots/nba/latest/1040x760/'
 const FILE_EXTENSION_PLAYER_IMAGE = 'png'
 
 const PlayerImage = ({ playerId, altText, className, overrideStyles }: PlayerImageProps) => {
+  const isMobile = useMediaQuery({ query: '(max-width: 760px)' })
+
   const style = {
-    ...defaultStyles,
+    ...getDefaultStyles(isMobile),
     ...overrideStyles,
   }
 
