2afe9d76397fd6b05e5e36a2d96ce249c154f971
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-10-05 19:26:42 -0300

    refactor: replace pure css by styled components

--- src/header/index.tsx
+++ src/header/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
 import background from './basketball-in-court-by-tjdragotta-from-unplash.png'
 
-const imageStyle = {
-  maxWidth: '100%',
-  opacity: 0.8
-}
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 15vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  max-height: 4rem;
+  overflow: hidden;
+`
+
+const HeaderImage = styled.img`
+  max-width: 100%;
+  opacity: 0.8;
+`
 
 const Header = () => (
-  <header className="App-header">
-    <img src={background} style={imageStyle}/>
-  </header>
+  <AppHeader>
+    <HeaderImage src={background} />
+  </AppHeader>
 )
 
 export default Header
