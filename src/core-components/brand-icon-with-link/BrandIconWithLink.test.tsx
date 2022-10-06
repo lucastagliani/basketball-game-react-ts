9652f142ec133a8d2b657fd4fcdc6fd237e2e203
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-10-06 02:45:23 -0300

    feat(icon-brand-with-link): creates component with font awesome

---
+++ src/core-components/brand-icon-with-link/BrandIconWithLink.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import BrandIconWithLink from '.'
+
+describe('<BrandIconWithLink />', () => {
+  it('should render link with aria label provided', () => {
+    const props = {
+      faCode: 'github',
+      ariaLabelText: 'Github',
+      url: 'http://github.com'
+    }
+    render(<BrandIconWithLink {...props} />)
+    expect(screen.getByRole('link', {name: 'Github'})).toBeInTheDocument()
+  })
+})
