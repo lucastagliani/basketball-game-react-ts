12f24fb7540bda39edee5888c87dc5f3cf32e26d
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-10-10 00:25:39 -0300

    feat: replaces styled-components by emotion

--- src/footer/index.tsx
+++ src/footer/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled from '@emotion/styled'
 import SocialMedia from './SocialMedia'
 
 const AppFooter = styled.footer`
--- src/header/index.tsx
+++ src/header/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled from '@emotion/styled'
 import background from './basketball-in-court-by-tjdragotta-from-unplash.png'
 
 const AppHeader = styled.header`
