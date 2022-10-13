fc5377241eb6f3a0d8b331c093314cfe7416e865
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-10-13 01:25:23 -0300

    refactor(footer): adjust footer for mobile view

--- src/footer/index.tsx
+++ src/footer/index.tsx
@@ -2,57 +2,75 @@ import React from 'react'
 import styled from '@emotion/styled'
 import SocialMedia from './SocialMedia'
 import CheckoutCodeText from './CheckoutCodeText'
+import { useMediaQuery } from 'react-responsive'
 
-const AppFooter = styled.footer`
+type ContainerProps = {
+  isMobile?: boolean
+}
+
+const AppFooter = styled.footer<ContainerProps>`
   width: 100%;
   background-color: #afb4ba;
   padding: 1rem;
   display: flex;
-  flex-direction: row;
+  flex-direction: ${({isMobile}) => isMobile ? 'column' : 'row' };
   bottom: 0;
 `
 
 const Text = styled.span`
-  font-size: 11px
+  font-size: 12px;
 `
 
-const Box = styled.div`
-  width: 33.33%;
-  margin: auto;
-`
+const Box = styled.div<ContainerProps>(
+  ({isMobile}) => {
+    return {
+      width: isMobile ? '100%' : '33.33%',
+      margin: 'auto',
+      ...(isMobile && {
+        padding: '0.25rem'
+      })
+    }
+  }
+)
+
+
+
+const socialMediaItems = [
+  {
+    faCode: 'linkedin',
+    url: 'https://www.linkedin.com/in/lucastagliani/'
+  },
+  {
+    faCode: 'medium',
+    url: 'https://medium.com/@lucastagliani'
+  },
+  {
+    faCode: 'github',
+    url: 'https://github.com/lucastagliani'
+  },
+  {
+    faCode: 'instagram',
+    url: 'http://instagram.com/lucas_tagliani'
+  },
+  {
+    faCode: 'twitter',
+    url: 'https://twitter.com/lucastagliani'
+  },
+]
 
 const Footer = (): JSX.Element => {
-  const socialMediaItems = [
-    {
-      faCode: 'linkedin',
-      url: 'https://www.linkedin.com/in/lucastagliani/'
-    },
-    {
-      faCode: 'medium',
-      url: 'https://medium.com/@lucastagliani'
-    },
-    {
-      faCode: 'github',
-      url: 'https://github.com/lucastagliani'
-    },
-    {
-      faCode: 'instagram',
-      url: 'http://instagram.com/lucas_tagliani'
-    },
-    {
-      faCode: 'twitter',
-      url: 'https://twitter.com/lucastagliani'
-    },
-  ]
+  const isMobile = useMediaQuery({ query: '(max-width: 760px)' })
+  
+  // TODO: avoid isMobile prop being passed so much
   return (
-    <AppFooter>
-      <Box>
+    <AppFooter isMobile={isMobile}>
+      <Box isMobile={isMobile}>
         <CheckoutCodeText />
       </Box>
-      <Box>
+      <Box isMobile={isMobile}>
         <Text>Copyright @ Lucas Tagliani Aguiar</Text>
       </Box>
-      <Box>
+      <Box isMobile={isMobile}>
         <SocialMedia items={socialMediaItems} />
       </Box>
     </AppFooter>
