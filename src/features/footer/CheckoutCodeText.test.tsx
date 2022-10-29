9e8b9bb4e542c0cefbc3b238c14a1bb6997dcb73
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-10-29 02:03:31 -0300

    test(footer): add tests to CheckoutCodeText

---
+++ src/features/footer/CheckoutCodeText.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import CheckoutCodeText from './CheckoutCodeText'
+
+describe('<CheckoutCodeText />', () => {
+  beforeEach(() => {
+    render(<CheckoutCodeText />)
+  })
+
+  it('renders pre text', () => {
+    expect(screen.getByText(/checkout the code behind:/i)).toBeInTheDocument()
+  })
+
+  it('renders UI repo link', () => {
+    expect(screen.getByRole('link', { name: 'UI' })).toHaveAttribute(
+      'href',
+      'https://github.com/lucastagliani/nba-info-react-ts',
+    )
+  })
+
+  it('renders API repo link', () => {
+    expect(screen.getByRole('link', { name: 'API' })).toHaveAttribute(
+      'href',
+      'https://github.com/lucastagliani/nba-api-nodejs',
+    )
+  })
+})
