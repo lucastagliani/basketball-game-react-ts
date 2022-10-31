26d3d02ce725d280ce3891637c1d782886e9adf0
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-10-31 00:58:09 -0300

    test(player-name-game): track play_again on GameOverModal

--- src/features/player-name-game/GameOverModal/GameOverModal.test.tsx
+++ src/features/player-name-game/GameOverModal/GameOverModal.test.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import GameOverModal from '.'
+import userEvent from '@testing-library/user-event'
+
+const trackSpy = jest.fn()
+
+jest.mock('../../useTrackingService', () => {
+  return jest.fn(() => ({
+    track: trackSpy,
+  }))
+})
 
 describe('<GameOverModal />', () => {
   beforeEach(() => {
@@ -24,5 +33,14 @@ describe('<GameOverModal />', () => {
 
   it('should render modal button', () => {
     expect(screen.getByRole('button', { name: 'Play again!' })).toBeInTheDocument()
+  })
+
+  it('calls track with play_again when button clicked', () => {
+    userEvent.click(screen.getByRole('button'))
+    expect(trackSpy).toHaveBeenCalledTimes(1)
+    expect(trackSpy).toHaveBeenCalledWith('play_again', {
+      total_answers: 5,
+      total_right_answers: 4,
+    })
   })
 })
