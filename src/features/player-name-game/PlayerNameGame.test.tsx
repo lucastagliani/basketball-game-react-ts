22739a760a35abf1e908f74f958869bab74a70d9
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-11-01 03:04:05 -0300

    feat(player-name-game): update user answer track contract

--- src/features/player-name-game/PlayerNameGame.test.tsx
+++ src/features/player-name-game/PlayerNameGame.test.tsx
@@ -16,6 +16,17 @@ jest.mock('axios', () => ({
   },
 }))
 
+const trackSpy = jest.fn()
+
+jest.mock('../useTrackingService', () => {
+  return jest.fn(() => ({
+    track: trackSpy,
+  }))
+})
+
+const getRightAnswerButton = () => screen.getByText('Nikola Jokic')
+const getWrongAnswerButton = () => screen.getByText('Franz Wagner')
+
 describe('<PlayerNameGame />', () => {
   it('should render a game title', async () => {
     render(<PlayerNameGame />)
@@ -26,32 +37,68 @@ describe('<PlayerNameGame />', () => {
       expect(gameTitle).toBeInTheDocument()
     })
   })
+
   it('should render a player picture', async () => {
     render(<PlayerNameGame />)
     await waitFor(() => {
       expect(screen.getByRole('img', { name: 'player' })).toBeInTheDocument()
     })
   })
+
   it('should render 4 possible answers', async () => {
     render(<PlayerNameGame />)
     await waitFor(() => {
       expect(screen.getAllByRole('button').length).toBe(4)
     })
   })
-  it('should give feedback to user when answer is right', async () => {
+
+  it('should give visual feedback to user when answer is right', async () => {
     render(<PlayerNameGame />)
     await waitFor(() => {
-      const rightAnswer = screen.getByText('Nikola Jokic')
-      userEvent.click(rightAnswer)
-      expect(screen.getByText('1 / 1')).toBeInTheDocument()
+      userEvent.click(getRightAnswerButton())
+    })
+    expect(screen.getByText('1 / 1')).toBeInTheDocument()
+  })
+
+  it('should give visual feedback to user when answer is incorrect', async () => {
+    render(<PlayerNameGame />)
+    await waitFor(() => {
+      userEvent.click(getWrongAnswerButton())
+    })
+    expect(screen.getByText('0 / 1')).toBeInTheDocument()
+  })
+
+  it('should track user answer', async () => {
+    render(<PlayerNameGame />)
+    await waitFor(() => {
+      userEvent.click(getWrongAnswerButton())
+    })
+    expect(trackSpy).toBeCalledTimes(1)
+    expect(trackSpy).toBeCalledWith('user_answer', {
+      is_correct: false,
+      attempt_number: 1,
     })
   })
-  it('should give feedback to user when answer is incorrect', async () => {
+
+  it('should track user end game after 5 attempts (2 corrects)', async () => {
     render(<PlayerNameGame />)
+    const userAnswers = [
+      'Nikola Jokic',
+      'John Collins',
+      'Franz Wagner',
+      'Kawhi Leonard',
+      'Nikola Jokic',
+    ]
+
     await waitFor(() => {
-      const rightAnswer = screen.getByText('Kawhi Leonard')
-      userEvent.click(rightAnswer)
-      expect(screen.getByText('0 / 1')).toBeInTheDocument()
+      userAnswers.forEach(userAnswer => {
+        userEvent.click(screen.getByText(userAnswer))
+      })
+    })
+
+    expect(trackSpy).toBeCalledWith('end_game', {
+      total_right_answers: 2,
+      total_answers: 5,
     })
   })
 })
