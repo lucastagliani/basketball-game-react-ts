1ee35ae1b5c6b4e54c942410a6659d47ec4a9b9f
Lucas Tagliani <lucas.tagliani@outlook.com>
2022-11-15 03:08:16 -0300

    refactor(Score): replace style with css from emotion

--- src/core-components/Score/index.tsx
+++ src/core-components/Score/index.tsx
@@ -1,15 +1,17 @@
-import React, { CSSProperties } from 'react'
+/** @jsxImportSource @emotion/react */
+import { CSSObject } from '@emotion/react'
+import React from 'react'
 
 type ScoreProps = {
   attempts: number
   correct: number
   textBeforeScore?: string
-  textStyles?: CSSProperties
-  scoreStyles?: CSSProperties
-  containerStyles?: CSSProperties
+  textStyles?: CSSObject
+  scoreStyles?: CSSObject
+  containerStyles?: CSSObject
 }
 
-const defaultContainerStyles: CSSProperties = {
+const defaultContainerStyles: CSSObject = {
   padding: '1rem',
   fontSize: '1.6rem',
   color: '#333',
@@ -24,9 +26,9 @@ const Score = ({
   containerStyles,
 }: ScoreProps): JSX.Element => {
   return (
-    <div style={{ ...defaultContainerStyles, ...containerStyles }}>
-      {textBeforeScore && <span style={textStyles}>{`${textBeforeScore} `}</span>}
-      <span aria-label="score" style={scoreStyles}>{`${correct} / ${attempts}`}</span>
+    <div css={{ ...defaultContainerStyles, ...containerStyles }}>
+      {textBeforeScore && <span css={textStyles}>{`${textBeforeScore} `}</span>}
+      <span aria-label="score" css={scoreStyles}>{`${correct} / ${attempts}`}</span>
     </div>
   )
 }
